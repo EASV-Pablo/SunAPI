11d7c296e5bac7bf9002cd5f44ddce36a8c16ce4
EASV-Pablo <pabl0311@easv365.dk>
2022-03-13 12:32:41 +0100

    a

--- Logic/LogicSun.cs
+++ Logic/LogicSun.cs
@@ -11,17 +11,19 @@ namespace SunAPI.Logic
     {
         public OutputDto calculateSunrise(InputDto input)
         {
-            DateTime date = DateTime.ParseExact(input.date, "dd/MM/yyyy", CultureInfo.InvariantCulture);
-            string tz = TimeZoneLookup.GetTimeZone(input.Latitude, input.Logitude).Result;
-            TimeSpan ts = TZConvert.GetTimeZoneInfo(tz).BaseUtcOffset;
-            Coordinate c = new Coordinate(input.Latitude, input.Logitude, date);
-            c.Offset = ts.Hours;
+            Coordinate c = createCoordinateObj(input);
 
             return new OutputDto { Hour = c.CelestialInfo.SunRise.ToString(), Type = "Sunrise", Machine = Program.name };
-
         }
 
         public OutputDto calculateSunset(InputDto input)
+        {
+            Coordinate c = createCoordinateObj(input);
+
+            return new OutputDto { Hour = c.CelestialInfo.SunSet.ToString(), Type = "Sunset", Machine = Program.name };
+        }
+
+        public Coordinate createCoordinateObj(InputDto input) 
         {
             DateTime date = DateTime.ParseExact(input.date, "dd/MM/yyyy", CultureInfo.InvariantCulture);
             string tz = TimeZoneLookup.GetTimeZone(input.Latitude, input.Logitude).Result;
@@ -29,8 +31,7 @@ namespace SunAPI.Logic
             Coordinate c = new Coordinate(input.Latitude, input.Logitude, date);
             c.Offset = ts.Hours;
 
-            return new OutputDto { Hour = c.CelestialInfo.SunSet.ToString(), Type = "Sunset", Machine = Program.name };
-
+            return c;
         }
     }
 }
